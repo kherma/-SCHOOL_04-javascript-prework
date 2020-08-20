f0a695503076dd44a910f92659a07816a24de1fd
kherma <kamil.herma1@gmail.com>
2020-08-20 13:12:01 +0200

    Read computer move

--- js/script.js
+++ js/script.js
@@ -1,4 +1,15 @@
-let computerMove = 'kamień';
-let playerMove = 'papier';
+// Generate random number
+let calculations = Math.floor(Math.random() * 3 + 1);
 
-printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to '+ playerMove +', to wygrywasz!');
+console.log('Wylosowana liczba to: ' + calculations);
+
+//Transfer Random number into string
+let computerMove = 'Nieznany ruch';
+
+if (calculations == 1) {
+    computerMove = 'kamień';
+} else if (calculations == 2) {
+    computerMove = 'papier';
+} else {
+    computerMove = 'nożyce';
+}
